03c88ab600bfa3c33172e17972b34d2e32b32d65
davewang0125 <wang.dave@gmail.com>
2020-10-03 10:18:40 -0700

    demo commit

--- demo2.py
+++ demo2.py
@@ -11,4 +11,4 @@ for i in range(1, 10):
         break
     print(i)
 else:
-    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
+    print("this is not printed because for break but not due to fail in condition")
