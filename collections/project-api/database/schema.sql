f335dcaffd89b33a8cbc6805c11fbd61cce1dac5
jianiye <cosmoyjn@gmail.com>
2022-11-22 16:33:03 -0800

    add schema and customer.py

--- collections/project-api/database/schema.sql
+++ collections/project-api/database/schema.sql
@@ -1,8 +1,47 @@
--- your table ddl etc are in here. 
+CREATE DATABASE meetings;
+USE meetings;
 
-create table customer 
-  column 
+CREATE TABLE users (
+id int,
+username varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
 
+CREATE TABLE meetings (
+id int,
+startTime datetime NOT NULL,
+endTime datetime NOT NULL,
+location varchar(255) DEFAULT "Virginia",
+location_id int DEFAULT 1,
+host_id int,
+PRIMARY KEY (id),
+FOREIGN KEY (host_id) REFERENCES users(id)
+-- FOREIGN KEY (location_id) REFERENCES servers(location_id)
+);
 
+CREATE TABLE participants (
+    participant_id int,
+    meeting_id int,
+    start_time datetime NOT NULL,
+    end_time datetime NOT NULL,
+    is_host bool,
+    FOREIGN KEY (participant_id) REFERENCES users(id),
+    FOREIGN KEY (meeting_id) REFERENCES meetings(id),
+    CHECK (start_time < end_time)
+);
+
+CREATE TABLE transcripts (
+meeting_id int,
+speaker_id int,
+start_time datetime,
+end_time datetime,
+content text,
+audio mediumtext,
+content_lang varchar(255) DEFAULT "english",
+FOREIGN KEY (speaker_id) REFERENCES users(id),
+FOREIGN KEY (meeting_id) REFERENCES meetings(id),
+CHECK (start_time < end_time)
+);
 
 
--- collections/project-api/server/customer.py
+++ collections/project-api/server/customer.py
@@ -1,25 +1,111 @@
 """ jiani, customer """
+import mysql.connector
+import json
+from flask import Flask, request, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+import sys  
 
-import sys   
+app = Flask(__name__)
+api = Api(app)
 
+meetingdb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="meeting"
+)
+cursor = meetingdb.cursor() 
 
-class Customer:
+# set table contents for API contact
+parser = reqparse.RequestParser()
+parser.add_argument('username',  location='args')
+parser.add_argument('email', location='args')
+parser.add_argument('id', type=int, location='args')
+
+# get table terms in userkey
+cursor.execute("desc users")
+userkeys = [column[0] for column in cursor.fetchall()]
+
+class Customer(Resource):
     """ customer class """
    
-    def get(self, text ):
-        return translate_text(text) 
+    def get(self, id):
+        query = ("SELECT * FROM users where id=%s;" % (id))
+        cursor.execute(query)
+        for content in cursor:
+            userInfo = {k:c for k, c in zip(userkeys, content)}
+            userjson = json.dumps(userInfo)
+        return userjson
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
+    def put(self, id):
         args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
+        try:
+            updateContents = ""
+            for k, v in args.items():
+                if v:
+                    if k == "id":
+                        updateContents+=k+"="+str(v)+","
+                    else:
+                        updateContents+=k+"='"+v+"',"
+            updateContents = updateContents[:-1]
+            updatequery = "UPDATE users SET " + updateContents + " WHERE id=%s;" % (id)
+            cursor.execute(updatequery)
+        except:
+            # if id not found then create new
+            insertKeys = "(id, "
+            insertVals = "(%s, " %id
+            for k, v in args.items():
+                if v:
+                    insertKeys += k+","
+                    if k == "id":
+                        continue
+                    else:
+                        insertVals += "'"+v+"'"+","
+            insertKeys = insertKeys[:-1]+")"
+            insertVals = insertVals[:-1]+");"
+            insertquery = "INSERT INTO users "+  insertKeys + " VALUES " + insertVals
+            cursor.execute(insertquery)
+        return "", 201
+
+    def delete(self, id):
+        query = ("DELETE FROM users where id=%s;" % (id))
+        cursor.execute(query)
+        return '', 201
 
  
-# table sql save the customer.
-# api create, read, update, delete. 
+class Customers(Resource):
+    def get(self):
+        query = ("SELECT * FROM users;")
+        cursor.execute(query)
+        contents = []
+        for content in cursor:
+            contents.append({k:c for k, c in zip(userkeys, content)})
+
+        contentsjson = json.dumps(contents)
+        return contentsjson
+
+    def post(self, id):
+        args = parser.parse_args()
+        insertKeys = "(id, "
+        insertVals = "(%s, " %id
+        for k, v in args.items():
+            if v:
+                insertKeys += k+","
+                if k == "id":
+                    continue
+                else:
+                    insertVals += "'"+v+"'"+","
+        insertKeys = insertKeys[:-1]+")"
+        insertVals = insertVals[:-1]+");"
+        insertquery = "INSERT INTO users "+  insertKeys + " VALUES " + insertVals
+        cursor.execute(insertquery)
+        return "", 201
+
+api.add_resource(Customers, '/customers')
+api.add_resource(Customer, '/customer/<id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    cursor.close()
+    meetingdb.close()
